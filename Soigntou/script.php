e41fbb130bdf0ca5571f0fe873d52e4ba6f92bae
davidclochard <davidclochard77@gmail.com>
2014-12-09 09:25:17 +0100

    ajout script + creation map activity

---
+++ Soigntou/script.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+$jsonUploaded = file_get_contents("fromData.json");
+
+$jsonDecoded = json_decode($jsonUploaded);
+
+foreach($jsonDecoded as $item){
+	unset($item->{'datasetid'});
+	unset($item->{'recordid'});
+	unset($item->{'geometry'});
+	unset($item->{'record_timestamp'});
+	unset($item->{'fields'}->{'departement'});
+	unset($item->{'fields'}->{'libdepartement'});
+	unset($item->{'fields'}->{'wgs'});
+	unset($item->{'fields'}->{'dateautor'});
+	unset($item->{'fields'}->{'datemaj'});
+	unset($item->{'fields'}->{'dateouv'});
+	
+	
+}
+ 
+$jsonReturned=json_encode($jsonDecoded , JSON_FORCE_OBJECT);
+
+$jsonFile = fopen("data.json", "x+");
+fputs($jsonFile, $jsonReturned);
+fclose($jsonFile);
+
+
+?>
+
+
---
+++ Soigntou/src/org/davy/soigntou/MapActivity.java
@@ -0,0 +1,7 @@
+package org.davy.soigntou;
+
+public class MapActivity {
+	
+	
+
+}
